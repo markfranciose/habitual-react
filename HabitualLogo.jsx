ae4e7b1c1dfeb67f18bd7cd8ad4fc0eacfe26883
Mark Franciose <mark.franciose@gmail.com>
2017-09-16 21:43:32 -0500

    bring in logo, main-page header, habits list, new habit button, topbar

---
+++ HabitualLogo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HabitualLogo = () => (
+	<div id='habitual-logo'>
+		<span id='logo-text'>Habitual</span>
+	</div>	
+)
+
+export default HabitualLogo;
---
+++ MyHabitsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyHabitsList = (props) => (
+	<div id="habits-list-container">
+		<h3>This will be a habits list!</h3>
+	</div>	
+)
+
+export default MyHabitsList;
